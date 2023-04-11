bef075baa04c6c185f9a4a9dfe121ce77856d910
kawaops <santiago_avendano82162@elpoli.edu.co>
2023-04-11 15:39:29 -0500

    cambio para ver las notificaciones de slack

--- billing/src/main/java/com/paymentchain/billing/controller/InvoiceRestController.java
+++ billing/src/main/java/com/paymentchain/billing/controller/InvoiceRestController.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ * /
 package com.paymentchain.billing.controller;
 
 import com.paymentchain.billing.common.InvoiceRequestMapper;
