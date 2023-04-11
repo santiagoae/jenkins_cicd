f34e18473d98ca01bd95e5aa667364bfcda0318d
kawaops <santiago_avendano82162@elpoli.edu.co>
2023-04-11 14:12:43 -0500

    correccion de archivos para el test

--- billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-20T12:52:21+0200",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
+    date = "2023-04-11T13:52:11-0500",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_362 (Private Build)"
 )
 @Component
 public class InvoiceResposeMapperImpl implements InvoiceResposeMapper {
